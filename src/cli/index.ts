e1862ce35086ac99984478b975c992b5b0c135fa
Jade <c4llm3Jade@gmail.com>
2026-07-01 17:46:06 +0800

    chore: add dotenv for api key loading

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "dotenv/config";
 import { Command } from "commander";
 import { registerInitCommand } from "./commands/init.js";
 import { registerMapCommand } from "./commands/map.js";
